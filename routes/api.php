bea4fedbe1da39a43b6c2e7ade44875dc7a64f37
harshita-innostax <harshita.gupta@innostax.com>
2025-02-06 18:40:40 +0530

    Added Model and Seeder files

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -17,38 +18,37 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
-Route::get('/posts',function(){
-    $data = include(app_path('Data/data.php'));
-
-    return json_encode($data);
+Route::get('/users', function () {
+    User::create(["email" => "harshita@gmail.com", "name" => "harshita", "password" => "123"]);
+    $users = User::all();
+    return json_encode($users);
 });
 
-Route::delete('/posts/{id}',function($id){
+Route::delete('/user/{id}', function ($id) {
     $posts = include(app_path('Data/data.php'));
-    $filteredPosts = array_values(array_filter($posts,function($post) use ($id){
+    $filteredPosts = array_values(array_filter($posts, function ($post) use ($id) {
         return $post['id'] != $id;
     }));
     return json_encode([
-        'message'=> 'Post deleted successfully',
+        'message' => 'Post deleted successfully',
         'posts' => $filteredPosts
     ]);
 
-    
+
 });
 
-Route::put('/posts/{id}',function( Request $request,$id){
+Route::put('/user/{id}', function (Request $request, $id) {
     $posts = include(app_path('Data/data.php'));
 
-    foreach($posts as &$post){
-        if($post['id']==$id){
-            $post['title']=$request->input('title',$post['title']);
-            $post['body']=$request->input('body',$post['body']);
+    foreach ($posts as &$post) {
+        if ($post['id'] == $id) {
+            $post['title'] = $request->input('title', $post['title']);
+            $post['body'] = $request->input('body', $post['body']);
         }
     }
     return json_encode([
-        'message'=> 'Post updated successfully',
+        'message' => 'Post updated successfully',
         'posts' => $posts
     ]);
 });
 
-    
